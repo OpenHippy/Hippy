90252d6d116879005d3b3741c4f3b617b398ae8c
zealotchen <zealotchen@tencent.com>
2025-08-14 11:50:05 +0800

    feat: rename README.md

--- docs/assets/js/docsify.js
+++ docs/assets/js/docsify.js
@@ -403,7 +403,7 @@
     return new RegExp(("\\.(" + (ext.replace(/^\./, '')) + "|html)$"), 'g').test(path)
       ? path
       : /\/$/g.test(path)
-        ? (path + "README" + ext)
+        ? (path + "HOME" + ext)
         : ("" + path + ext);
   }
 
@@ -425,7 +425,7 @@
 
     path = config.alias ? getAlias(path, config.alias) : path;
     path = getFileName(path, ext);
-    path = path === ("/README" + ext) ? config.homepage || path : path;
+    path = path === ("/HOME" + ext) ? config.homepage || path : path;
     path = isAbsolutePath(path) ? path : getPath(base, path);
 
     if (isRelative) {
@@ -2552,7 +2552,7 @@
         subMaxLevel: 0,
         loadSidebar: null,
         loadNavbar: null,
-        homepage: 'README.md',
+        homepage: 'HOME.md',
         coverpage: '',
         basePath: '',
         auto2top: false,
@@ -7694,7 +7694,7 @@
         !config.ignore
       ) {
         if (href === compilerClass.config.homepage) {
-          href = 'README';
+          href = 'HOME';
         }
 
         href = router.toURL(href, null, router.getCurrentPath());
--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -314,11 +314,11 @@
         }
       }
       var isExistHome = paths.indexOf(namespaceSuffix + '/') === -1;
-      var isExistReadme = paths.indexOf(namespaceSuffix + '/README') === -1;
+      var isExistReadme = paths.indexOf(namespaceSuffix + '/HOME') === -1;
       if (isExistHome && isExistReadme) {
         paths.unshift(namespaceSuffix + '/');
       }
-    } else if (paths.indexOf('/') === -1 && paths.indexOf('/README') === -1) {
+    } else if (paths.indexOf('/') === -1 && paths.indexOf('/HOME') === -1) {
       paths.unshift('/');
     }
 
